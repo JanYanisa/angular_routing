393da9cf4752f7100860503e60879a0066192b36
yanisa.p <yanisa.p@gofive.co.th>
2022-11-07 21:13:35 +0700

    add children path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,21 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
-  { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UserComponent },
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id', component: ServerComponent },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [{ path: ':id/:name', component: UserComponent }],
+  },
+
+  {
+    path: 'servers',
+    component: ServersComponent,
+    children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent },
+    ],
+  },
   { path: '', component: HomeComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent },
 ];
 @NgModule({
   declarations: [
